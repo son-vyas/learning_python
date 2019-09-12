970b2863c147e36fe521ad1995645dff752e0547
Soniya VYas <soniyavyas29@gmail.com>
2019-09-12 19:15:40 +0530

    Added code to add optional arguments

--- nameargparse.py
+++ nameargparse.py
@@ -3,3 +3,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('name', default=True)
 parser.add_argument('hold', action='store', nargs='+')
 name_group = parser.add_mutually_exclusive_group()
+name_group.add_argument('--firstname', action='store_true',
+                        help='print first name using argparse')
+name_group.add_argument('--lastname', action='store_true',
+                        help="print last name using argparse")
