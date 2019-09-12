a9f7139cd0515ba6215e8b07dc8b609ca90136e6
Soniya VYas <soniyavyas29@gmail.com>
2019-09-12 19:14:51 +0530

    Added code to make the optional arguments mutually exclusive

--- nameargparse.py
+++ nameargparse.py
@@ -2,3 +2,4 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('name', default=True)
 parser.add_argument('hold', action='store', nargs='+')
+name_group = parser.add_mutually_exclusive_group()
