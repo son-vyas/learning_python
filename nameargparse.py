da0a798cc36387676a34bc60339efce59faf1c55
Soniya VYas <soniyavyas29@gmail.com>
2019-09-12 19:18:22 +0530

    Added code to parse the arguments and join the list of arguments which have been holded

--- nameargparse.py
+++ nameargparse.py
@@ -7,3 +7,5 @@ name_group.add_argument('--firstname', action='store_true',
                         help='print first name using argparse')
 name_group.add_argument('--lastname', action='store_true',
                         help="print last name using argparse")
+args = parser.parse_args()
+my_string = ' '.join(parser.parse_args().hold)
